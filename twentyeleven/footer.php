9527dd6b9b4963b2c84a27c782b28002ee48ea39
Sara Abbasi <sara@shecod.es>
2018-03-16 17:27:48 -0400

    first commit

---
+++ twentyeleven/footer.php
@@ -0,0 +1,33 @@
+    </div><!-- id="main"-->
+
+
+    <footer>
+
+    <div class="TGIT-footer__page-links__items">
+        <div class="TGIT-footer__page-links__items__wrapper">
+
+            <?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+
+        </div><!-- class="TGIT-footer__page-links__items__wrapper" -->
+
+    </div><!-- class="TGIT-footer__page-links__items" -->
+
+    <div class="TGIT-footer__trademark clearfix">
+        <div class="TGIT-footer__trademark__wrapper">
+            <div class="copyright-notice">
+                © 2016 Maven Ventures, All Rights Reserved.
+            </div>
+            <div class="design-credit">
+                Site by <a href="http://www.secondlanguagedesign.com/" target="_blank"> Second Language Design</a>
+            </div>
+        </div><!-- class="TGIT-footer__trademark" -->
+    </div><!-- class="TGIT-footer__trademark clearfix" -->
+
+
+    </footer><!-- #colophon -->
+
+    <?php wp_footer(); ?>
+
+</body>
+
+</html>
